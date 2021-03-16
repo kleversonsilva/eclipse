13e0bd54f8aa901f0951e291022e7d22271c5b09
kleversonsilva <jose.kleverson@dcx.ufpb.br>
2021-03-16 17:43:53 -0300

    Adcionei uma Linha

--- Hello_Github/src/br/com/kleverson/git/HelloGuiit.java
+++ Hello_Github/src/br/com/kleverson/git/HelloGuiit.java
@@ -3,6 +3,7 @@ public class HelloGuiit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELOO GUIT 2021!!");
+		System.out.println("Kleverson Aluno de SI.");
 		
 		
 		
